f63e004833c350dca14159ea4c9338335e8e07af
Alexander <aleksievisa@gmail.com>
2023-01-21 23:40:32 +0300

    add media fn

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -4,28 +4,30 @@ import { UserEntity } from "../user/user.entity";
 import { Repository } from "typeorm";
 import { JwtService } from "@nestjs/jwt";
 import { AuthDto } from "./auth.dto";
-import { compare, genSalt, hash } from "bcryptjs";
+import {compare, genSalt, hash} from "bcryptjs"
 
 @Injectable()
 export class AuthService {
-  constructor(
-    @InjectRepository(UserEntity)
-    private readonly userRepository: Repository<UserEntity>,
-    private readonly jwtService: JwtService
-  ) {}
 
+  constructor(@InjectRepository(UserEntity) private readonly userRepository: Repository<UserEntity>,
+              private readonly jwtService: JwtService
+  ) { }
   async login(dto: AuthDto) {
+
     const user = await this.validateUser(dto)
 
-    return {
-      user: this.returnUserField(user),
+    return{
+      user: this.returnUserFields(user),
       accessToken: await this.issueAccessToken(user.id)
     }
+
   }
 
   async register(dto: AuthDto) {
-    const oldUser = await this.userRepository.findOneBy({email: dto.email})
-    if (oldUser) throw new BadRequestException('Email занят')
+    const oldUser = await this.userRepository.findOneBy({email:dto.email})
+    if(oldUser) throw new BadRequestException(
+      'Пользователь с таким email уже существует'
+    )
 
     const salt = await genSalt(10)
 
@@ -36,42 +38,46 @@ export class AuthService {
 
     const user = await this.userRepository.save(newUser)
 
-    return {
-      user: this.returnUserField(user),
+    return{
+      user: this.returnUserFields(user),
       accessToken: await this.issueAccessToken(user.id)
     }
+
   }
 
-  async validateUser(dto: AuthDto) {
+
+  async validateUser(dto:AuthDto){
     const user = await this.userRepository.findOne({
       where: {
-        email: dto.email
+        email:dto.email
       },
       select: ['id', 'email', 'password']
     })
 
-    if (!user) throw new NotFoundException('Пользователь не найден!')
+    if(!user) throw new NotFoundException("Пользователь не найден!")
 
     const isValidPassword = await compare(dto.password, user.password)
-    if (!isValidPassword) throw new UnauthorizedException('Не правильный пароль')
+    if(!isValidPassword) throw new UnauthorizedException("Не правильный пароль")
 
     return user
   }
+
 
   async issueAccessToken(userId: number) {
     const data = {
       id: userId
     }
-
     return await this.jwtService.signAsync(data, {
-      expiresIn: '31d'
+      expiresIn: "31d"
     })
   }
 
-  returnUserField(user: UserEntity) {
+
+  returnUserFields(user: UserEntity) {
     return {
-      id: user.id,
-      email: user.email
+      id:user.id,
+      email:user.email
     }
   }
+
 }
--- src/auth/strategies/jwt.strategy.ts
+++ src/auth/strategies/jwt.strategy.ts
@@ -1,10 +1,12 @@
+import { Injectable } from "@nestjs/common";
 import { PassportStrategy } from "@nestjs/passport";
-import { Strategy, ExtractJwt } from 'passport-jwt'
 import { ConfigService } from "@nestjs/config";
 import { InjectRepository } from "@nestjs/typeorm";
 import { UserEntity } from "../../user/user.entity";
 import { Repository } from "typeorm";
-import { Injectable } from "@nestjs/common";
+import {ExtractJwt, Strategy} from "passport-jwt"
+
+
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
@@ -16,11 +18,12 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: true,
-      secretOrKey: configService.get('JWT_SECRET')
+      secretOrKey: configService.get("JWT_SECRET")
     });
   }
-  
-  async validate({ id }: Pick<UserEntity, 'id'>) {
-    return this.userRepository.findBy({ id })
+
+  async validate({id}: Pick<UserEntity, "id">) {
+    return this.userRepository.findBy({id})
   }
+
 }
